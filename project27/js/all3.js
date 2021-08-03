67f3ba4366a2567db45b579bd0ebffa30a731c42
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-03 16:31:54 +0800

    index3.html 讀取陣列資料，並賦予新變數流程

--- project27/js/all3.js
+++ project27/js/all3.js
@@ -3,6 +3,10 @@
 let colors = ["blue", "red", "black"]
 
 // 讀取陣列資料﹑了解陣列長度
-console.log(colors[0]);
+// console.log(colors[0]);
 
-console.log(colors[1]);
+// console.log(colors[1]);
+
+let likeColor = colors[2];
+
+console.log(likeColor);

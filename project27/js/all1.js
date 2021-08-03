28b490a3893182daf96ee55edafc089e7d7e851b
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-03 16:16:31 +0800

    project 27 陣列教學，index1 認識陣列以及物件

---
+++ project27/js/all1.js
@@ -0,0 +1,12 @@
+// 陣列
+let colors = ['red', 'yellow', 'black'];
+
+
+// 物件
+let home = {
+    "fatherName": "Tom",
+    "motherName": "Mary",
+    "sons": ["Bob", "John", "Tim"],
+    "isWakeUp": false,
+    "dogs":3,
+}

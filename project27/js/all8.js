e32d112d603b51bc73bc34601e076dc0d0d14423
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-03 17:40:00 +0800

    新增 index8.html : pop 與 shift 刪除資料

---
+++ project27/js/all8.js
@@ -0,0 +1,14 @@
+// 陣列新增語法 push、unshift
+// 陣列刪除語法 pop、shift
+
+
+let colors = ['red', 'blue', 'black','pink','yellow','green'];
+console.log('原始資料 : ',colors);
+
+// 刪除陣列最後一筆資料 pop()
+colors.pop();
+console.log('刪除陣列最後一筆資料',colors);
+
+// 刪除陣列第一筆資料 shift()
+colors.shift();
+console.log('刪除陣列第一筆資料',colors);

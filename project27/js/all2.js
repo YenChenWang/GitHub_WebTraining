3b3e0b023d3b5496305bf506c754757aa5d8eafb
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-03 16:26:24 +0800

    index2 陣列也能放數字與混合資料

--- project27/js/all2.js
+++ project27/js/all2.js
@@ -2,4 +2,13 @@ let color1 = "blue";
 let color2 = "red";
 
 // 陣列
-let colors = ["blue","red","yellow"]
+let colors = ["blue", "red", "yellow"]
+
+let ary = ["blue", 5, false];
+console.log(ary);
+
+let books = [5, 30, 400, 100];
+console.log(books);
+
+let data = [];
+console.log(data);

9e954bd56967865fc912ae4e4be04ff5973af7e7
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-03 17:12:14 +0800

    新增 index6.html : push 寫入資料流程

---
+++ project27/js/all6.js
@@ -0,0 +1,12 @@
+// 陣列寫入資料  push
+
+let colors = ['blue', 'red', 'black'];
+
+console.log(colors);
+
+
+colors.push(3);
+colors.push("pink");
+colors.push("yellow");
+
+console.log('新增資料後 : ', colors);

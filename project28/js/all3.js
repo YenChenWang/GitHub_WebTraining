02394af82f4e2e7bb461a5df8e4e6fd3b729332b
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-04 13:53:30 +0800

    新增 index3.html : 新增物件屬性

---
+++ project28/js/all3.js
@@ -0,0 +1,13 @@
+let home = {};
+
+// 新增物件屬性
+home.motherName = "mary";
+home.fatherName = "Jack"
+
+console.log(home);
+
+// 屬性 : value 值,
+//     motherName: "Mary",
+//     fatherName: "Tom",
+//     dogs: 3,
+//     isWakeUp:false

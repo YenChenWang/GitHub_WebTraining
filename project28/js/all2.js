ad31ec2ca6732f7900872fc1edab35afbbe71957
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-04 13:47:35 +0800

    新增 index2.html : 讀取物件屬性的值

---
+++ project28/js/all2.js
@@ -0,0 +1,16 @@
+// 物件
+let home = {
+    // 屬性 : value 值,
+    motherName: "Mary",
+    fatherName: "Tom",
+    dogs: 3,
+    isWakeUp:false
+}
+
+
+console.log(home);
+// 讀取物件屬性的值
+console.log(home.motherName);
+
+let tomDogs = home.dogs;
+console.log(tomDogs);

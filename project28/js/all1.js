2335094739f7003089c9d504a3ca7fa4cf90dab5
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-04 13:43:26 +0800

    新增 index1.html : 物件格式

---
+++ project28/js/all1.js
@@ -0,0 +1,19 @@
+// 陣列
+
+let colors = ['blue', 'red', 'black'];
+
+
+
+// 描述家庭
+let motherName = "Mary";
+let fatherName = "Tom";
+let dogs = 3;
+
+// 物件
+let home = {
+    // 屬性 : value 值,
+    motherName: "Mary",
+    fatherName: "Tom",
+    dogs: 3,
+    isWakeUp:false
+}

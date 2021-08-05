ef4529baab2a2501256fae26f69caf11bc6b19a4
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-05 18:26:23 +0800

    PJ29 index1.html : 陣列與物件混和使用

---
+++ project29/js/all1.js
@@ -0,0 +1,22 @@
+// 有兩個水果攤
+// 第一個水果攤，老闆 Tom，香蕉有 6 個，蘋果有 5 個，還沒關店
+// 第二個水果攤，老闆 John，香蕉有 9 個，蘋果有 8 個，還沒關店
+
+// 陣列混合物件用法
+
+let market = [
+    {
+        bossName: "Tom",
+        bananaNum: 6,
+        appleNum: 5,
+        isClose:false
+    },
+    {
+        bossName:"John",
+        bananaNum: 9,
+        appleNum: 8,
+        isClose:false
+    }
+]
+
+console.log(market);
